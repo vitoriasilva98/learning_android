4f7341074df3806cab65455a3af9511d7220f562
Vitoria da Silva <vspinto@stefanini.com>
2022-08-31 14:15:55 -0300

    prova c1

---
+++ Projetos/Praticando exercicios da faculdade/MeuIMC/app/src/main/java/br/com/vsep/meuimc/MainActivity.kt
@@ -0,0 +1,11 @@
+package br.com.vsep.meuimc
+
+import android.app.Activity
+import android.os.Bundle
+
+class MainActivity : Activity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
